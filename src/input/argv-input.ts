a9a1b3cc275f91f7320f6cbefdcb137b9d19c848
Marcus Pöhls <marcus.poehls@gmail.com>
2021-04-20 22:38:37 +0200

    add comments

--- src/input/argv-input.ts
+++ src/input/argv-input.ts
@@ -1,7 +1,7 @@
 'use strict'
 
-import minimist from 'minimist'
 import { tap } from '@supercharge/goodies'
+import minimist, { ParsedArgs } from 'minimist'
 
 export class ArgvInput {
   /**
@@ -12,10 +12,7 @@ export class ArgvInput {
   /**
    * The parsed input tokens.
    */
-  private parsed: {
-    _: string[]
-    [key: string]: unknown
-  }
+  private parsed: ParsedArgs
 
   constructor (argv?: string[]) {
     this.tokens = argv ?? process.argv.slice(2)
@@ -45,10 +42,41 @@ export class ArgvInput {
     return this.parsed._[0]
   }
 
+  /**
+   * Returns the provided input arguments.
+   *
+   * @returns {String[]}
+   *
+   * @example
+   * ```bash
+   * # Imagine this command input
+   * $ ./cli command:name UserModel --dry-run -h
+   *
+   * # the input from above results in these arguments
+   * ['command:name', 'UserModel']
+   * ```
+   */
   arguments (): string[] {
     return this.parsed._.slice(1)
   }
 
+  /**
+   * Returns the provided input options.
+   *
+   * @returns {Object}
+   *
+   * @example
+   * ```bash
+   * # Imagine this command input
+   * $ ./cli command:name --dry-run -h
+   *
+   * # the input from above results in these options
+   * {
+   *   h: true,
+   *   'dry-run': true
+   * }
+   * ```
+   */
   options (): {[key: string]: unknown} {
     const { _, ...rest } = this.parsed
 
