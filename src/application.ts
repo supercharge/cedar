8c59efaeef19d8d3fef86ddab6a2f2b39c4e2bc8
Marcus Pöhls <marcus.poehls@gmail.com>
2021-04-23 16:33:43 +0200

    wip

--- src/application.ts
+++ src/application.ts
@@ -1,5 +1,6 @@
 'use strict'
 
+import kleur from 'kleur'
 import { Command } from './command'
 import { tap } from '@supercharge/goodies'
 import { ArgvInput } from './input/argv-input'
@@ -11,9 +12,21 @@ interface ApplicationMeta {
    */
   name: string
 
+  /**
+   * Stores the application’s version.
+   */
+  version?: string
+
+  /**
+   * The command list.
+   */
   commands: Command[]
 }
+
 export class Application {
+  /**
+   * Stores the application’s meta data, like name, version, commands.
+   */
   private readonly meta: ApplicationMeta
 
   /**
@@ -48,6 +61,28 @@ export class Application {
    */
   name (): string {
     return this.meta.name
+  }
+
+  /**
+   * Set the application’s version.
+   *
+   * @param {String} version
+   *
+   * @returns {Application}
+   */
+  setVersion (version: string): this {
+    return tap(this, () => {
+      this.meta.version = version
+    })
+  }
+
+  /**
+   * Returns the application’s version.
+   *
+   * @returns {String}
+   */
+  version (): string {
+    return this.meta.version ?? ''
   }
 
   /**
@@ -112,16 +147,58 @@ export class Application {
    * @throws
    */
   async run (input?: string[]): Promise<void> {
-    const argv = new ArgvInput(input)
+    const argv = new ArgvInput(input).parse({
+      alias: this.defaultAliases()
+    })
+
+    if (argv.hasOption('version')) {
+      return this.outputVersion()
+    }
+
+    const commandName = argv.firstArgument()
 
     const command = this.commands().find(command => {
-      return command.name() === argv.firstArgument()
+      return command.name() === commandName
     })
 
     if (command) {
       return await command.run(argv)
     }
 
-    throw new Error(`No command registered with name "${String(argv.firstArgument())}"`)
+    if (commandName) {
+      throw new Error(`No command registered with name "${commandName}"`)
+    }
+
+    await this.defaultCommand().run(argv)
+  }
+
+  /**
+   * Returns the list of default aliases, like `help: 'h'` and `version: 'v'`.
+   *
+   * @returns {Object}
+   */
+  defaultAliases (): {[key: string]: string|string[]} {
+    return { help: 'h', version: 'v' }
+  }
+
+  /**
+   * Print the application version to the console.
+   */
+  outputVersion (): void {
+    if (this.name() && this.version()) {
+      return console.log(`${this.name()} ${kleur.green(this.version())}`)
+    }
+
+    if (this.name()) {
+      console.log(this.name())
+    }
+
+    if (this.version()) {
+      console.log(kleur.green(this.version()))
+    }
+  }
+
+  defaultCommand (): Command {
+    return this.meta.defaultCommand
   }
 }
