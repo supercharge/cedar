ebe7b13d80e4c0f2664cb2dc518fcac506bb54b6
Marcus Pöhls <marcus.poehls@gmail.com>
2021-04-27 20:05:44 +0200

    version

--- example/example.js
+++ example/example.js
@@ -4,7 +4,7 @@ const { Application } = require('../dist')
 const HelloCommand = require('./hello-command')
 
 async function run () {
-  const app = new Application('Supercharge Console Example').setVersion('v1.2.3')
+  const app = new Application('Supercharge Console Example').setVersion('v1.1.3')
 
   app
     .register('make:migration')
