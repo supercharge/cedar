97752e510219a60ee31b8104356c2218e7d7b9f5
Marcus Pöhls <mp@seda.digital>
2021-04-30 16:37:55 +0200

    wip ConsoleOutput

--- src/io/console-output.ts
+++ src/io/console-output.ts
@@ -1,52 +1,122 @@
 'use strict'
 
 import kleur, { Kleur } from 'kleur'
+import { tap } from '@supercharge/goodies'
 
 export class ConsoleOutput {
+  /**
+   * Returns the colors instance.
+   *
+   * @returns {Kleur}
+   */
+  colors (): Kleur {
+    return kleur
+  }
+
+  /**
+   * Log an empty line to the console. Useful if you want to create some space to breath.
+   *
+   * @returns {ConsoleOutput}
+   */
+  emptyLine (): this {
+    return this.log('')
+  }
+
   /**
    * Log the given `message` to the console or create a chain for colored output.
    *
    * @param message
    *
-   * @return {undefined | Kleur}
+   * @returns {ConsoleOutput}
    */
-  log (message?: undefined | null): Kleur
-  log (message: string): void
-  log (message: any): any {
-    if (message === undefined || message === null) {
-      return kleur
-    }
-
-    console.log(message)
+  log (message: string): this {
+    return tap(this, () => {
+      console.log(message)
+    })
   }
 
-  error (message: string | Error): void {
+  /**
+   * Log the given warning `info` to the terminal.
+   *
+   * @param {String} message
+   *
+   * @returns {ConsoleOutput}
+   */
+  debug (message: string): this {
     if (typeof message === 'string') {
-      this.log(
-        `${this.log().red().bold(' ERROR ')} ${message}`
-      )
+      return this.log(`${this.colors().blue().bold(' DEBUG ')} ${message}`)
     }
 
-    this.log(
-      `${this.log().red().bold(' error ')} ${(message as Error).message}\n`
-    )
-    this.log(
-      `${this.log().bgRed().white().bold(' error ')} ${(message as Error).message}\n`
-    )
-    this.log(
-      `${this.log().red().bold(' ERROR ')}  ${(message as Error).message}\n`
-    )
-    this.log(
-      `${this.log().bgRed().white().bold(' ERROR ')}  ${(message as Error).message}`
-    )
+    throw new Error(`Unsupported input when logging a warning. Received: ${typeof message}`)
+  }
 
-    const lines = (message as Error).stack?.split('\n')
+  /**
+   * Log the given warning `info` to the terminal.
+   *
+   * @param {String} message
+   *
+   * @returns {ConsoleOutput}
+   */
+  info (message: string): this {
+    if (typeof message === 'string') {
+      return this.log(`${this.colors().cyan().bold(' INFO ')} ${message}`)
+    }
+
+    throw new Error(`Unsupported input when logging a warning. Received: ${typeof message}`)
+  }
+
+  /**
+   * Log the given warning `message` to the terminal.
+   *
+   * @param {String} message
+   *
+   * @returns {ConsoleOutput}
+   */
+  warn (message: string): this {
+    if (typeof message === 'string') {
+      return this.log(`${this.colors().yellow().bold(' WARN ')} ${message}`)
+    }
+
+    throw new Error(`Unsupported input when logging a warning. Received: ${typeof message}`)
+  }
+
+  /**
+   * Log the given `error` to the terminal. The `error` can be a string or an error instance.
+   *
+   * @param {String|Error} error
+   *
+   * @returns {ConsoleOutput}
+   */
+  error (error: string | Error): this {
+    if (typeof error === 'string') {
+      return this.log(`${this.colors().red().bold(' ERROR ')} ${error}`)
+    }
+
+    if (error instanceof Error) {
+      return this
+        .log(`${this.colors().bgRed().white().bold(' ERROR ')}  ${error.message}`)
+        .log(this.formatStack(error.stack))
+    }
+
+    throw new Error(`Unsupported input when logging an error. Received: ${typeof error}`)
+  }
+
+  /**
+   * Returns the a formatted error stack, if available.
+   *
+   * @param {String} stack
+   *
+   * @returns {String}
+   */
+  private formatStack (stack?: string): string {
+    if (!stack) {
+      return ''
+    }
+
+    return stack
+      .split('\n')
       .splice(1)
-      .map((line: string) => {
-        return `${this.log().dim(line)}`
-      })
+      .map(line => `${this.colors().dim(line)}`)
       .join('\n')
-
-    this.log(this.log().dim(lines ?? ''))
   }
 }
