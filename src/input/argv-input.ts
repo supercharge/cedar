ebd20bb49481e3ff1cadd431f0b032c1cce3b4ab
Marcus Pöhls <mp@seda.digital>
2021-05-04 17:45:51 +0200

    extends Input

--- src/input/argv-input.ts
+++ src/input/argv-input.ts
@@ -1,11 +1,10 @@
 'use strict'
 
-import Map from '@supercharge/map'
+import { Input } from './input'
 import { tap } from '@supercharge/goodies'
-import { InputDefinition } from './input-definition'
 import minimist, { ParsedArgs, Opts as Options } from 'minimist'
 
-export class ArgvInput {
+export class ArgvInput extends Input {
   /**
    * The input arguments. By default `process.argv.slice(2)`
    */
@@ -17,22 +16,15 @@ export class ArgvInput {
   private parsed: ParsedArgs
 
   /**
-   * The parsed input tokens.
-   */
-  private readonly meta: {
-    options: Map<string, unknown>
-  }
-
-  /**
    * Create a new instance for the given `args`.
    *
    * @param args
    */
   constructor (args?: string[]) {
+    super()
+
     this.args = args ?? process.argv.slice(2)
     this.parsed = { _: [] }
-
-    this.meta = { options: new Map() }
   }
 
   /**
@@ -41,35 +33,12 @@ export class ArgvInput {
   parse (options?: Options): this {
     return tap(this, () => {
       this.parsed = minimist(this.args, options ?? {})
+      this.assignParsedInput()
     })
   }
 
-  /**
-   * Bind the given input `definition` with the provided input options and arguments.
-   *
-   * @param {InputDefinition} definition
-   *
-   * @returns {ArgvInput}
-   *
-   * @param definition
-   */
-  public bind (_definition: InputDefinition): this {
+  private assignParsedInput (): void {
     // TODO
-
-    return this
-  }
-
-  /**
-   * Validate the command line input against the definition.
-   *
-   * @returns {ArgvInput}
-   *
-   * @throws
-   */
-  public validate (): this {
-    // TODO
-
-    return this
   }
 
   /**
@@ -79,82 +48,5 @@ export class ArgvInput {
    */
   firstArgument (): string {
     return this.parsed._[0] ?? ''
-  }
-
-  /**
-   * Returns the provided input arguments.
-   *
-   * @returns {String[]}
-   *
-   * @example
-   * ```bash
-   * # Imagine this command input
-   * $ ./cli command:name UserModel --dry-run -h
-   *
-   * # the input from above results in these arguments
-   * ['command:name', 'UserModel']
-   * ```
-   */
-  arguments (): string[] {
-    return this.parsed._.slice(1)
-  }
-
-  /**
-   * Returns the provided input options.
-   *
-   * @returns {Object}
-   *
-   * @example
-   * ```bash
-   * # Imagine this command input
-   * $ ./cli command:name --dry-run -h
-   *
-   * # the input from above results in these options
-   * {
-   *   h: true,
-   *   'dry-run': true
-   * }
-   * ```
-   */
-  options (): Map<string, unknown> {
-    if (this.meta.options.isEmpty()) {
-      const { _, ...rest } = this.parsed
-
-      Object
-        .entries({ ...rest })
-        .forEach(([key, value]) => {
-          this.meta.options.set(key, value)
-        })
-    }
-
-    return this.meta.options
-  }
-
-  /**
-   * Returns the option value for the given `name`.
-   */
-  option (name: string): unknown {
-    return this.options().get(name)
-  }
-
-  /**
-    * Set an option for the given `name` and assign the `value`.
-    *
-    * @param {String} name
-    * @param {*} value
-    */
-  setOption (name: string, value: unknown): this {
-    return tap(this, () => {
-      this.meta.options.set(name, value)
-    })
-  }
-
-  /**
-    * Determine whether an option with the given `name` exists.
-    *
-    * @param {String} name
-    */
-  hasOption (name: string): boolean {
-    return this.options().has(name)
   }
 }
