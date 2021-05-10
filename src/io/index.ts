4eb8ca4e0f7437184a8a7a53c9435ba4347a4046
Marcus Pöhls <mp@seda.digital>
2021-05-10 10:55:35 +0200

    exports

---
+++ src/io/index.ts
@@ -0,0 +1,10 @@
+'use strict'
+
+export * from './builder/base'
+export * from './builder/prompt'
+
+export * from './console-input'
+export * from './console-input-contracts'
+export * from './console-output'
+
+export * from './tag'
