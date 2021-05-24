32ff97868b89a3dc1492bc7c0cffa2c11d0b20d9
Marcus Pöhls <marcus.poehls@gmail.com>
2021-05-24 06:56:25 +0200

    test

--- test/application.js
+++ test/application.js
@@ -3,8 +3,20 @@
 const { test } = require('tap')
 const { Application } = require('../dist')
 
-test('Application', async t => {
-  await t.pass(() => {
-    return new Application().run(['test-command', '--name=Marcus', '-a 30', '-v'])
+test('Console Application', async () => {
+  test('version', async t => {
+    const app = new Application()
+
+    app.setVersion('1.1.2')
+    t.equal(app.version(), '1.1.2')
+
+    app.setVersion()
+    t.equal(app.version(), '')
+
+    app.setVersion(null)
+    t.equal(app.version(), '')
+
+    app.setVersion(4)
+    t.equal(app.version(), 4)
   })
 })
