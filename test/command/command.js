57d2d07f8401d28b14471a65af3137498c9c51e0
Marcus Pöhls <mp@seda.digital>
2021-05-28 16:22:54 +0200

    add tests

--- test/command/command.js
+++ test/command/command.js
@@ -1,11 +1,171 @@
 'use strict'
 
+const Sinon = require('sinon')
 const { test } = require('tap')
-const { Application, Command } = require('../../dist')
+const { Application, Command, InputArgumentBuilder, InputOptionBuilder } = require('../../dist')
 
 test('Command', async () => {
   test('constructor', async t => {
     const command = new Command('test:command')
     t.equal(command.getName(), 'test:command')
   })
+
+  test('name', async t => {
+    const command = new Command().name('test:command')
+    t.equal(command.getName(), 'test:command')
+  })
+
+  test('description', async t => {
+    const command = new Command()
+    t.equal(command.hasDescription(), false)
+
+    command.description('test command')
+    t.equal(command.hasDescription(), true)
+    t.equal(command.getDescription(), 'test command')
+  })
+
+  test('throws when a command does not have a name', async t => {
+    const app = new Application()
+    const command = new Command('')
+
+    t.throws(() => {
+      app.add(command)
+    })
+  })
+
+  test('setApplication', async t => {
+    const app = new Application()
+    const command = new Command('')
+    t.throws(() => command.application())
+
+    command.setApplication(app)
+    t.equal(command.application(), app)
+  })
+
+  test('addArgument returns the argument builder instance', async t => {
+    const command = new Command('')
+    const argument = command.addArgument('name')
+
+    t.equal(argument instanceof InputArgumentBuilder, true)
+    t.equal(command.definition().hasArgument('name'), true)
+  })
+
+  test('addArgument returns command when using the argument builder', async t => {
+    const command = new Command('')
+    const result = command.addArgument('name', builder => { })
+
+    t.equal(result instanceof Command, true)
+    t.equal(command.definition().hasArgument('name'), true)
+  })
+
+  test('throws when not providing an argument name', async t => {
+    const command = new Command('')
+    t.throws(() => command.addArgument())
+  })
+
+  test('argument', async t => {
+    const command = new Command('')
+    command.addArgument('name').defaultValue('Supercharge')
+    t.equal(command.argument('name'), 'Supercharge')
+  })
+
+  test('throws when accessing an undefined argument', async t => {
+    const command = new Command()
+    t.throws(() => command.argument('name'), 'The argument "name" does not exist in command "Command"')
+  })
+
+  test('addOption returns the option builder instance', async t => {
+    const command = new Command('')
+    const option = command.addOption('dry-run')
+
+    t.equal(option instanceof InputOptionBuilder, true)
+    t.equal(command.definition().hasOption('dry-run'), true)
+  })
+
+  test('addOption returns command when using the argument builder', async t => {
+    const command = new Command('')
+    const result = command.addOption('dry-run', builder => { })
+
+    t.equal(result instanceof Command, true)
+    t.equal(command.definition().hasOption('dry-run'), true)
+  })
+
+  test('throws when not providing an option name', async t => {
+    const command = new Command('')
+    t.throws(() => command.addOption(), 'Missing option name in command "Command"')
+  })
+
+  test('option', async t => {
+    const command = new Command('')
+    command.addOption('dry-run').defaultValue('whynot')
+    t.equal(command.option('dry-run'), 'whynot')
+  })
+
+  test('throws when accessing an undefined argument', async t => {
+    const command = new Command()
+    t.throws(() => command.option('dry-run'), 'The option "dry-run" does not exist in command "Command"')
+  })
+
+  test('is disabled', async t => {
+    const command = new HiddenCommand('')
+    t.equal(command.isEnabled(), false)
+  })
+
+  test('throws when run is not implemented', async t => {
+    const command = new Command()
+    t.throws(() => command.run(), 'You must implement the "run" method in your "Command" command')
+  })
+
+  test('run', async t => {
+    const processExitStub = Sinon.stub(process, 'exit').returns()
+
+    let ran = false
+
+    class TestCommand extends Command {
+      configure () {
+        this.name('test:command')
+      }
+
+      run () { ran = true }
+    }
+
+    await new Application().add(
+      new TestCommand()
+    ).run(['test:command'])
+
+    t.equal(ran, true)
+
+    processExitStub.restore()
+  })
+
+  // test('throws when the command does not define the given argument', async t => {
+  //   const processExitStub = Sinon.stub(process, 'exit').returns()
+
+  //   let ran = false
+
+  //   class TestCommand extends Command {
+  //     configure () {
+  //       this.name('test:command')
+  //     }
+
+  //     run () { ran = true }
+  //   }
+
+  //   await t.rejects(() => {
+  //     return new Application().add(
+  //       new TestCommand()
+  //     ).run(['test:command', 'argument1'])
+  //   })
+
+  //   t.equal(ran, false)
+  //   t.equal(processExitStub.calledWith(1), true)
+
+  //   processExitStub.restore()
+  // })
 })
+
+class HiddenCommand extends Command {
+  isEnabled () {
+    return false
+  }
+}
